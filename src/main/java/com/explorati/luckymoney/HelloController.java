31113d6f9eff886fcbc4f15d5e2fa185ff52fbf2
explorati <w1748419447@163.com>
2019-10-20 17:08:38 +0800

    对象配置的加载及测试

--- src/main/java/com/explorati/luckymoney/HelloController.java
+++ src/main/java/com/explorati/luckymoney/HelloController.java
@@ -1,13 +1,18 @@
 package com.explorati.luckymoney;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @Autowired
+    private LimitConfig limitConfig;
+
 //        @RequestMapping(value="/hello", method= RequestMethod.GET)
     @GetMapping("/hello")
     public String say(){
-        return "learning SpringBoot!";
+        return "说明: " + limitConfig.getDescription();
     }
 }
