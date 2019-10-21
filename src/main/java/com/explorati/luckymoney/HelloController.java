132ad10ef8431bda7e298699e4bd7af97016b0e3
explorati <w1748419447@163.com>
2019-10-21 10:40:31 +0800

    将application.yml分为两个子文件，根据需要在application.yml中指定启用的文件

--- src/main/java/com/explorati/luckymoney/HelloController.java
+++ src/main/java/com/explorati/luckymoney/HelloController.java
@@ -2,17 +2,21 @@ package com.explorati.luckymoney;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("hello")
 public class HelloController {
 
     @Autowired
     private LimitConfig limitConfig;
 
-//        @RequestMapping(value="/hello", method= RequestMethod.GET)
-    @GetMapping("/hello")
-    public String say(){
-        return "说明: " + limitConfig.getDescription();
+    @GetMapping("/say")
+//    @RequestMapping("say")
+    public String say(@RequestParam(value="id", required = false, defaultValue = "0") Integer id){
+//        return "说明: " + limitConfig.getDescription();
+        return "id: " + id;
     }
 }
