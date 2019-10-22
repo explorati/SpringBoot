4aa1b94549e7ba5009bfa7a20858df2c3567f595
explorati <w1748419447@163.com>
2019-10-22 20:08:44 +0800

    DAO层 Hibernate只需要继承JpaRepository

---
+++ src/main/java/com/explorati/luckymoney/LuckymoneyRepository.java
@@ -0,0 +1,12 @@
+package com.explorati.luckymoney;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @ Author : Weijian_Wang
+ * @ Date : Created in 18:58 2019/10/22 0022
+ * @ Description ：
+ */
+public interface LuckymoneyRepository extends JpaRepository<Luckymoney,Integer> {
+
+}
